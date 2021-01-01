86832b906dec7adca6b1b8398f25b6298cfc48fc
waasnipun <waasnipun@gmail.com>
2021-01-01 18:13:47 +0530

    chnge

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,8 +68,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
+        name: `hexcodeblogs`,
+        short_name: `hexcodeblogs`,
         description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
         start_url: `/`,
         background_color: `#fff`,
